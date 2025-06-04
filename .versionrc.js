85c99225556947d916c32386bf293189782d37f1
rainlong <ylongwangi@qq.com>
2025-06-04 17:05:48 +0800

    chore: 修复了除fix和feat外还会触发release的问题

--- .versionrc.js
+++ .versionrc.js
@@ -1,14 +1,17 @@
 module.exports = {
-  types: [
-    { type: 'feat', section: '✨ Features' },
-    { type: 'fix', section: '🐛 Bug Fixes' },
-    { type: 'docs', section: '📝 Documentation' },
-    { type: 'style', section: '💄 Styles' },
-    { type: 'refactor', section: '♻️ Code Refactoring' },
-    { type: 'perf', section: '⚡️ Performance Improvements' },
-    { type: 'test', section: '✅ Tests' },
-    { type: 'ci', section: '👷 CI/CD' },
-    { type: 'chore', section: '🔧 Chores' },
-    { type: 'revert', section: '⏪️ Reverts' }
-  ]
-}; 
+    types: [
+      { type: 'fix', section: '🐛 Bug Fixes', hidden: false },
+      { type: 'feat', section: '✨ Features', hidden: false },
+  
+      // 以下类型设置 hidden: true，表示不会出现在 changelog 中，且不会影响版本更新
+      { type: 'docs', section: '📝 Documentation', hidden: true },
+      { type: 'style', section: '💄 Styles', hidden: true },
+      { type: 'refactor', section: '♻️ Code Refactoring', hidden: true },
+      { type: 'perf', section: '⚡️ Performance Improvements', hidden: true },
+      { type: 'test', section: '✅ Tests', hidden: true },
+      { type: 'ci', section: '👷 CI/CD', hidden: true },
+      { type: 'chore', section: '🔧 Chores', hidden: true },
+      { type: 'revert', section: '⏪️ Reverts', hidden: true }
+    ]
+  };
+  
